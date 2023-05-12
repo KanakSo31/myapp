01225365ce0486bf70e4f7c5d470ee6e25fbfa88
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-05-12 13:59:44 +0000

    home page

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,8 @@ const Home = ()=>{
             <div className="row">
                 <div className="col-10 mx-auto">
                     <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                        
+                        <h1>We are a Family of wisdom with great truths.</h1>
+                        <h2>I believe Buddhists have lost the way of the Buddha and lack a real basis of impact.</h2>
                     </div>
                 </div>
             </div>
