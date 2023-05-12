65fc7d8c6f15985d0830c38e33bc596b379d3362
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-05-12 14:01:36 +0000

    home page developing

--- src/Home.js
+++ src/Home.js
@@ -9,15 +9,16 @@ const Home = ()=>{
             <div className="row">
                 <div className="col-10 mx-auto">
                     <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                        <h1>We are a Family of wisdom with great truths.</h1>
-                        <h2>I believe Buddhists have lost the way of the Buddha and lack a real basis of impact.</h2>
+                        <h1>We are a Family of <strong>wisdom with great truths.</strong></h1>
+                        <h2 className='my-3'>I believe Buddhists have lost the way of the Buddha and lack a real basis of impact.</h2>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-  </>)
-}
+  </>
+  );
+};
 
 
 export default Home;
