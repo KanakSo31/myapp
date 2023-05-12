576cf69b11131880c22c4861c916df6f8354edb1
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-05-12 13:34:02 +0000

    nav Item

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,35 +8,35 @@ const Navbar = () => {
                 <div className="col-10 mx-auto">
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">EBuDHIJM</a>
+                    <NavLink className="navbar-brand" href="#">EBuDHIJM</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <NavLink className="nav-link active" aria-current="page" href="#">Home</NavLink>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <NavLink className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     About
-                                </a>
+                                </NavLink>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Mission</a></li>
-                                    <li><a className="dropdown-item" href="#">Vision</a></li>
+                                    <li><NavLink className="dropdown-item" href="#">Mission</NavLink></li>
+                                    <li><NavLink className="dropdown-item" href="#">Vision</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Service</a>
+                                <NavLink className="nav-link" href="#">Service</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Join Us</a>
+                                <NavLink className="nav-link" href="#">Join Us</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">News</a>
+                                <NavLink className="nav-link" href="#">News</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Contact</a>
+                                <NavLink className="nav-link" href="#">Contact</NavLink>
                             </li>
                         </ul>
                     </div>
