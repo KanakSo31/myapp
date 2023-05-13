1c06397e05f49dace06858bd14ea1ef303cd009c
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-05-13 08:47:23 +0000

    home page developing

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import emage from "../src/images/home."
+import emage from "../src/images/Church.gif"
 
 
 const Home = ()=>{
@@ -9,6 +9,7 @@ const Home = ()=>{
     <div className="container-fluid nav_bg">
             <div className="row">
                 <div className="col-10 mx-auto">
+                    <div className='row'>
                     <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
                         <h1>We are a Family of <strong className='brand-name'>wisdom with great truths.</strong></h1>
                         <h2 className='my-3'>We believe Buddhists have lost the way of the Buddha and lack a real basis of impact.</h2>
@@ -17,7 +18,8 @@ const Home = ()=>{
                         </div>
                     </div>
                     <div className='col-lg-6 order-1 order-lg-2 header-img'>
-                        <img src={} className='img-fluid animated' alt='home-image'>
+                        <img src={emage} className="img-fluid animated" alt='home-image'/>
+                    </div>
                     </div>
                 </div>
             </div>
