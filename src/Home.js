718ebb07d28c9585b96f507a0faee8ac6080d5ef
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-05-13 10:11:19 +0000

    home page developing

--- src/Home.js
+++ src/Home.js
@@ -10,15 +10,15 @@ const Home = ()=>{
             <div className="row">
                 <div className="col-10 mx-auto">
                     <div className='row'>
-                    <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                        <h1>We are a Family of <strong className='brand-name'>wisdom with great truths.</strong></h1>
-                        <h2 className='my-3'>We believe Buddhists have lost the way of the Buddha and lack a real basis of impact.</h2>
+                    <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                        <h1>We are a Family of <strong className='brand-name'>wisdom &<br/> great truths.</strong></h1>
+                        <h6 className='my-3'>We believe Buddhists have lost the way of the Buddha impact.</h6>
                         <div className='mt-3'>
                             <a href='google.com' className='btn-get-started'>Get Started</a>
                         </div>
                     </div>
-                    <div className='col-lg-6 order-1 order-lg-2 header-img'>
-                        <img src={emage} className="img-fluid animated" alt='home-image'/>
+                    <div className='col-lg-6 order-1 order-lg-2 header-img '>
+                        <img src={emage} className="img-fluid animated rounded-4" alt='home-image'/>
                     </div>
                     </div>
                 </div>
