6ac003c9a1d553a61eb9009a8ea6d1d6bc3f4f08
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-05-13 08:23:12 +0000

    home page developing

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,10 @@ const Home = ()=>{
             <div className="row">
                 <div className="col-10 mx-auto">
                     <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                        <h1>We are a Family of <strong>wisdom with great truths.</strong></h1>
+                        <h1>We are a Family of <strong className='brand-name'>wisdom with great truths.</strong></h1>
                         <h2 className='my-3'>We believe Buddhists have lost the way of the Buddha and lack a real basis of impact.</h2>
                         <div className='mt-3'>
-                            <a href='' className='btn-get-started'>Get Started</a>
+                            <a href='google.com' className='btn-get-started'>Get Started</a>
                         </div>
                     </div>
                 </div>
